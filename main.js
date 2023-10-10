dd0af07f9fdc1874fadbd455c46fb55a3df65f1e
Mourium Haque <mourium.haque@gmail.com>
2023-10-10 18:48:15 -0400

    V 11

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 /*----- constants -----*/
+    /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
-let board;
-let turn = 'X';
+    let board;
+    let turn = 'X';
+// new code below
+    let win;
+
 /*----- cached element references -----*/
-const squares = Array.from(document.querySelectorAll('#board div'));
+    const squares = Array.from(document.querySelectorAll('#board div'));
 
 // new code below
-const messages = document.querySelector('h2');
+    const messages = document.querySelector('h2');
 
 /*----- event listeners -----*/
     document.getElementById('board').addEventListener('click', handleTurn);
