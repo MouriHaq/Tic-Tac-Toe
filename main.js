dc35bc565726589288a94b7a144e1ee8ca1fefda
Mourium Haque <mourium.haque@gmail.com>
2023-10-09 23:35:57 -0400

    Version 6

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-console.log('Your JS is linked up. Be the person you needed when you were little.')
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
 function init() {
     board = [
@@ -17,7 +18,8 @@ function init() {
     };
 
     function render() {
-        board.forEach(function(mark, index){
-        console.log(mark, index);
+        board.forEach(function(mark, index) {
+        //this sets the text content of the square of the same position to the mark on the board. 
+        squares[index].textContent = mark;
         });
         };
